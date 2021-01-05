f986eee37888a6e594e61fe277122b6b753d0a26
Martin Wilk <wilk-martin@t-online.de>
2021-01-05 16:17:47 +0100

    fixed typo in benchmark.sh

--- starter_files/benchmark.sh
+++ starter_files/benchmark.sh
@@ -68,8 +68,8 @@
 # Processing:    92  137  28.3    151     176
 # Waiting:       92  137  28.3    151     176
 # Total:        114  160  28.0    172     199#
-URI=`cat settings.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["URI"]'`
-key=`cat settings.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["auth_key"]'`
+URI=`cat settings.json | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["URI"])'`
+key=`cat settings.json | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["auth_key"])'`
 echo "Endpoint URL: $URI"
 echo "Authentication Key: $key"
 ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer $key' $URI
